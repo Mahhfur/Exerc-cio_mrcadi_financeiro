ed25128592cb4c78c4ffc07b98f592a15c421065
Mahhfur <mari.f.snt@gmail.com>
2024-12-05 16:17:58 -0300

    ultimo

--- dez_acoes.py
+++ dez_acoes.py
@@ -31,12 +31,12 @@ def get_data(symbols, limit):
 
 # Requisições e concatenando os DataFrames
 df_1_3 = get_data('AAPL,TSLA,MSFT', 3)
-df_4_6 = get_data('AMZN,ABEV3,ITUB4', 3)
-df_7_9 = get_data('MCDC34,BALL,PETR4', 3)
-df10 = get_data('NVDC34', 1)
+df_4_6 = get_data('AMZN,ABEV,ITUB', 3)
+df_7_9 = get_data('LAME,VIVA', 2)
+df10 = get_data('NVDC', 1)
 
 # Exibindo os DataFrames
-print(df_1_3)
-print(df_4_6)
+#print(df_1_3)
+#print(df_4_6)
 print(df_7_9)
-print(df10)
+#print(df10)
