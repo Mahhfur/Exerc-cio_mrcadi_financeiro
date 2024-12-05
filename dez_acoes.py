07124eb29a181f91b3355f2c010ab5a342876f97
Mahhfur <mari.f.snt@gmail.com>
2024-12-05 15:25:51 -0300

    teste

--- dez_acoes.py
+++ dez_acoes.py
@@ -1,16 +1,42 @@
-import http.client, urllib.parse
+import http.client
+import urllib.parse
+import json
+import pandas as pd  # Importando pandas
 
-conn = http.client.HTTPSConnection('api.stockdata.org')
+# Função para fazer a requisição e retornar o DataFrame
+def get_data(symbols, limit):
+    conn = http.client.HTTPSConnection('api.stockdata.org')
 
-params = urllib.parse.urlencode({
-    'api_token': 'ujmUaTYeAdRGe5MyEPbIrvF4mevABivctOt1CrgU',
-    'symbols': 'AAPL,TSLA',
-    'limit': 50,
+    # Definindo os parâmetros
+    params = urllib.parse.urlencode({
+        'api_token': 'ujmUaTYeAdRGe5MyEPbIrvF4mevABivctOt1CrgU',
+        'symbols': symbols,
+        'limit': limit,
     })
 
-conn.request('GET', '/v1/news/all?{}'.format(params))
+    # Requisição GET para a API
+    conn.request('GET', '/v1/data/quote?' + params)
 
-res = conn.getresponse()
-data = res.read()
+    # Obtendo a resposta
+    res = conn.getresponse()
+    data = res.read()
 
-print(data.decode('utf-8'))
+    # Convertendo os dados JSON
+    json_data = json.loads(data.decode('utf-8'))
+
+    # Normalizando os dados JSON em um DataFrame do Pandas
+    df = pd.json_normalize(json_data['data'])
+
+    return df
+
+# Requisições e concatenando os DataFrames
+df_1_3 = get_data('AAPL,TSLA,MSFT', 3)
+df_4_6 = get_data('AMZN,ABEV3,ITUB4', 3)
+df_7_9 = get_data('MCDC34,BALL,PETR4', 3)
+df10 = get_data('NVDC34', 1)
+
+# Exibindo os DataFrames
+print(df_1_3)
+print(df_4_6)
+print(df_7_9)
+print(df10)
