6a6f17afa7d4689b4f0778fc03caecab8254fa21
Mahhfur <mari.f.snt@gmail.com>
2024-12-05 13:21:44 -0300

    comecoo

--- dez_acoes.py
+++ dez_acoes.py
@@ -1,2 +1,19 @@
-import pandas as pd
+import requests
+import json
 
+# Substitua pela sua chave da API
+api_key = 'GET https://api.stockdata.org/v1/data/intraday/adjusted HTTP/1.1'
+
+# A URL da API para obter dados (aqui estamos usando um exemplo de dados históricos)
+url = f'https://api.stockdata.org/v1/data/quote?symbols=AAPL&api_token={api_key}'
+
+# Fazendo a requisição GET para a API
+response = requests.get(url)
+
+# Verificando se a requisição foi bem-sucedida
+if response.status_code == 200:
+    # Convertendo a resposta para um formato JSON
+    data = response.json()
+    print(json.dumps(data, indent=4))  # Exibindo os dados de maneira formatada
+else:
+    print(f'Erro ao obter dados: {response.status_code}')
